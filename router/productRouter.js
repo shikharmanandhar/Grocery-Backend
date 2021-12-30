68e96c6a920eddeba98ce80f0d835fd760ab17cd
shikharmanandhar <shikharaa26@gmail.com>
2021-12-30 11:38:35 +0545

    view all products and only by vendor

--- router/productRouter.js
+++ router/productRouter.js
@@ -53,5 +53,53 @@ const pName= req.body.pName;
 })
 
 
+router.delete('/product/delete', auth.verifyVendor, function(req,res){
+    const pid=req.body.pid;
+    const vendorId=req.vendorInfo._id;
+
+    Product.deleteOne({pid:pid,vendorId:vendorId})
+    .then(
+        function(){
+            res.json({message:"Product Deleted"})
+        }
+    )
+    .catch(function(){
+        res.json({message:"something went wrong!"})
+    })
+})
+
+
+//to view all the products
+// no login required
+router.get('/product/all', function(req,res){
+    Product.find()
+    .then(function(result){
+        res.json(result)
+    })
+    .catch(function(){
+        res.json({
+            message:"something went wrong"
+        }
+        )
+    })
+})
+
+//to view products of only one vendor
+router.get('/product/vendorall',auth.verifyVendor,function(req,res){
+    const vendorId=req.vendorInfo._id;
+
+    Product.find(
+        {
+            vendorId:vendorId
+        }
+    )
+    .then(function(result){
+        res.json(result)
+    })
+    .catch(function(){
+        res.json({message:"someting went wrong"})
+    })
+})
+
 
 module.exports=router;
