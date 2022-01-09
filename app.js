cd93e704661cf380433b0bafe7a1cadf7e273993
shikharmanandhar <shikharaa26@gmail.com>
2022-01-09 22:25:08 +0545

    Functions changed

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
-const express= require("express");
-const cors=require("cors");
+const express = require("express");
+const app = express();
 
-const app= express();
-app.use(cors());
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+// Cloudinary added
+const fileUpload = require("express-fileupload");
+
+const errorMiddleware = require("./middleware/error")
+
+const dotenv = require("dotenv"); 
+//config
+dotenv.config({path: "backend/config/config.env"})
+
+// When you requred to use POST & PUT REQUESTS as in this request we are sendind data or json OBject to server not needed for DELETE & GET
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(cookieParser());
+//for Cloudinary Use
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(fileUpload());
+
+//Routs imports
+const product = require("./routes/productRouts")
+const user = require("./routes/userRouts")
+const order = require("./routes/orderRouts")
+const payment = require("./routes/paymentRouts")
 
 
-require("./database/database");
 
-const CustomerRoute=require("./router/customerRouter");
-const VendorRoute=require("./router/vendorRouter");
-const ProductRoute=require("./router/productRouter")
-app.use(CustomerRoute,VendorRoute,ProductRoute);
-
-
+//The app.use() function is used to mount the specified middleware function(s) at the path which is being specified. It is mostly used to set up middleware for your application.
+app.use("/api/v1", product);
+app.use("/api/v1/",user);
+app.use("/api/v1/",order);
+app.use("/api/v1/",payment)
 
 
-app.listen("90");
+
+//Middleware for error
+app.use(errorMiddleware)
+
+
+module.exports = app;
