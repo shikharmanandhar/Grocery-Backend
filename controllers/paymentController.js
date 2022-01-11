9f097ed9d679e4f63708ccaf5e565068e3aa4c2a
shikharmanandhar <shikharaa26@gmail.com>
2022-01-11 12:10:03 +0545

    payment controller edited

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -5,9 +5,9 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 exports.processPayment = catchAsyncError(async (req,res,next)=>{
     const myPayment = await stripe.paymentIntents.create({
         amount: req.body.amount,
-        currency: "inr",
+        currency: "Nrs",
         metadata: {
-            company: "EShopper",
+            company: "FoodCourt",
         },
     })
 
