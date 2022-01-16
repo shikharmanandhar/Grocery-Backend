3aa05391f985dbcca5b671f66c2d34fd946634b3
shikharmanandhar <shikharaa26@gmail.com>
2022-01-16 07:54:33 +0545

    updated product model

--- models/productModel.js
+++ models/productModel.js
@@ -33,7 +33,7 @@ const productSchema = new mongoose.Schema({
     ],
     category:{
         type:String,
-        required:[true, "Please Enter Product Catagory"],
+        required:[true, "Enter Product Catagory"],
     },
     Stock:{
         type:Number,
