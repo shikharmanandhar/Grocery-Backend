9ec0350d3a7f606021041a541150d6130345789d
shikharmanandhar <shikharaa26@gmail.com>
2022-01-20 09:17:48 +0545

    trying stripe api

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -16,8 +16,8 @@ exports.processPayment = catchAsyncError(async (req,res,next)=>{
 //API_KEY MAIN Use in frontend so we are sending it to frontend
 
 
-// exports.sendStripeApiKey = catchAsyncError(async (req,res,next)=>{
-//     res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY });
-// })
+exports.sendStripeApiKey = catchAsyncError(async (req,res,next)=>{
+    res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY });
+})
 
 
