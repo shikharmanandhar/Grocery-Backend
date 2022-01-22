9c382daa8c73e5bb361a2363cadb6b340548174d
shikharmanandhar <shikharaa26@gmail.com>
2022-01-22 19:18:38 +0545

    adding NRS. in every cash

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -5,7 +5,7 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 exports.processPayment = catchAsyncError(async (req,res,next)=>{
     const myPayment = await stripe.paymentIntents.create({
         amount: req.body.amount,
-        currency: "Nrs",
+        currency: "",
         metadata: {
             company: "FoodCourt",
         },
@@ -16,8 +16,8 @@ exports.processPayment = catchAsyncError(async (req,res,next)=>{
 //API_KEY MAIN Use in frontend so we are sending it to frontend
 
 
-exports.sendStripeApiKey = catchAsyncError(async (req,res,next)=>{
-    res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY });
-})
+// exports.sendStripeApiKey = catchAsyncError(async (req,res,next)=>{
+//     res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY });
+// })
 
 
