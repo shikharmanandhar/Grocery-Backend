b525794f037f738e7bd06e07fd18cc273e77e12a
shikharmanandhar <shikharaa26@gmail.com>
2022-01-26 19:19:34 +0545

    again adding stripe

--- routes/paymentRouts.js
+++ routes/paymentRouts.js
@@ -5,7 +5,7 @@ const { isAuthenticatedUser } = require("../middleware/auth");
 const {processPayment} = require("../controllers/paymentController") 
 
 router.route("/payment/process").post(isAuthenticatedUser, processPayment);
-// router.route("/stripeapikey").get(isAuthenticatedUser, sendStripeApiKey);
+router.route("/stripeapikey").get(isAuthenticatedUser, sendStripeApiKey);
 
 
 
