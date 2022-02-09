3e5c9c611d1776b12553ecb7fd8ced10f711cc50
shikharmanandhar <shikharaa26@gmail.com>
2022-02-09 18:55:35 +0545

    user testing

--- test/user.test.js
+++ test/user.test.js
@@ -23,7 +23,7 @@ describe('User Schema test anything', () => {
 };
 return User.create(Userdata)
     .then((pro_ret) => {
-    expect(pro_ret.name).toEqual('Gauri');
+    expect(pro_ret.name).toEqual('shikhar');
     expect(pro_ret.email).toEqual('gauri@gmail.com');
     //expect(pro_ret.password).toEqual('gauri@123');
     expect(pro_ret.avatar.public_id).toEqual(
